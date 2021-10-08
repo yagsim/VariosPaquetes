fc36190d8ecd0de97056433ed761ed2850871b6e
Damian Nogueiras <damian@danielcastelao.org>
2021-10-08 09:52:00 +0200

    share!!!

--- src/com/dam1/ejercicio1/Ejercicio1.java
+++ src/com/dam1/ejercicio1/Ejercicio1.java
@@ -3,7 +3,7 @@ package com.dam1.ejercicio1;
 public class Ejercicio1 {
 
     public static void main(String[] args) {
-        System.out.println("Ejercicio1");
+        System.out.println("Share on GitHub");
     }
 
 
