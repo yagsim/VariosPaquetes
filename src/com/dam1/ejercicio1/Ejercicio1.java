651b5ae284822e396d38a4b01558a379fb949467
Damian Nogueiras <damian@danielcastelao.org>
2021-10-13 14:05:48 +0200

    despues de creado Readme

--- src/com/dam1/ejercicio1/Ejercicio1.java
+++ src/com/dam1/ejercicio1/Ejercicio1.java
@@ -5,5 +5,6 @@ public class Ejercicio1 {
     public static void main(String[] args) {
         System.out.println("Share on GitHub");
         System.out.println("ej1");
+        System.out.println("readme");
     }
 }
